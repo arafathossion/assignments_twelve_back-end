193a136505a32363cabb990775f273801fe60726
arafathossion <arafathossionrahman@gmail.com>
2022-05-26 19:18:50 +0600

    tools api

--- index.js
+++ index.js
@@ -11,7 +11,27 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x8cmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("assignmentTwelve");
+        const manufacturerTools = database.collection("manufacturerTools");
+     
 
+
+        app.get('/tools', async (req, res) => {
+            const tools = await manufacturerTools.find().toArray()
+            res.send(tools);
+        })
+       
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
